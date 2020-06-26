e1e4b63f1bc698cadedc887e8bae1724e4ae8f73
Ruby <ski579579@naver.com>
2020-06-26 10:29:37 +0900

    paasta-deployment

---
+++ paasta-deployment/deploy-openstack-monitoring.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+bosh -e micro-bosh -d paasta -n deploy paasta-deployment-monitoring.yml \
+	-o operations/openstack.yml \
+	-o operations/use-compiled-releases.yml \
+	-o operations/use-haproxy.yml \
+	-o operations/use-haproxy-public-network.yml \
+	-o operations/use-compiled-releases-haproxy.yml \
+	-o operations/use-postgres.yml \
+	-o operations/use-compiled-releases-postgres.yml \
+	-o operations/rename-network-and-deployment.yml \
+	-o paasta-addon/paasta-monitoring.yml \
+	-o paasta-addon/use-compiled-releases-monitoring-agent.yml \
+	-o operations/addons/enable-component-syslog.yml \
+	-o operations/addons/use-compiled-releases-syslog.yml \
+	-l openstack-vars.yml \
+	-l ../common/common_vars.yml
