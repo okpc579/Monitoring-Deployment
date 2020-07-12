060604c78c56e1e6409bc851b3321e655f51dae5
Ruby <ski579579@naver.com>
2020-07-12 23:15:55 +0900

    final

--- logsearch/deploy-logsearch.sh
+++ logsearch/deploy-logsearch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 bosh -e micro-bosh -n -d logsearch deploy logsearch-deployment.yml \
-	-o use-compiled-releases-logsearch.yml \
 	-l logsearch-vars.yml \
 	-l ../../common/common_vars.yml
--- paasta-monitoring/deploy-paasta-monitoring.sh
+++ paasta-monitoring/deploy-paasta-monitoring.sh
@@ -2,6 +2,5 @@
 
 bosh -e micro-bosh -n -d paasta-monitoring deploy paasta-monitoring.yml  \
 	-o use-public-network-openstack.yml \
-	-o use-compiled-releases-paasta-monitoring.yml \
 	-l paasta-monitoring-vars.yml \
 	-l ../../common/common_vars.yml
