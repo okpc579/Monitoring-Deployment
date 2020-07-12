d3c31826324639ed9a45d3d14839a7e5e3071f3c
okpc579 <55691511+okpc579@users.noreply.github.com>
2020-07-12 23:32:05 +0900

    Delete deploy-pinpoint-vsphere.sh

--- paasta-pinpoint-monitoring/deploy-pinpoint-vsphere.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'y' | bosh -d paasta-pinpoint-service deploy paasta-pinpoint.yml\
-	-o use-public-network-vsphere.yml \
-	-l pinpoint-vars.yml \
-	-l ../../common/common_vars.yml \
-	-l pem.yml
